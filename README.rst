c3865fcdc5a1f353aadd0611cd8178c27adb19cc
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2018-05-31 13:01:02 +0300

    reformat with src as package dir

---
+++ README.rst
@@ -0,0 +1,40 @@
+============
+forests.theme
+============
+
+FORESTS Theme
+
+
+Installation
+------------
+
+Install forests.theme by adding it to your buildout::
+
+    [buildout]
+
+    ...
+
+    eggs =
+        forests.theme
+
+
+and then running ``bin/buildout``
+
+
+Contribute
+----------
+
+- Issue Tracker: https://github.com/eea/forests.theme/issues
+- Source Code: https://github.com/eea/forests.theme
+
+
+Support
+-------
+
+If you are having issues, please let us know.
+
+
+License
+-------
+
+The project is licensed under the GPLv2.
--- setup.py
+++ setup.py
@@ -1,38 +1,51 @@
-from setuptools import setup, find_packages
-import os
+# -*- coding: utf-8 -*-
+"""Installer for the forests.content package."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+long_description = '\n\n'.join([
+    open('README.rst').read(),
+    open('CONTRIBUTORS.rst').read(),
+    open('CHANGES.rst').read(),
+])
 
 version = '0.1'
 
-setup(name='forests.theme',
-      version=version,
-      description="",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
-      classifiers=[
+setup(
+    name='forests.theme',
+    version=version,
+    description="FORESTS theme",
+    long_description=long_description,
+    # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Environment :: Web Environment",
         "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+    ],
+    keywords='Python Plone',
+    author='"EEA: IDM2 S-Team"',
+    author_email='Christian.Prosperini@eea.europa.eu',
+    url='https://pypi.python.org/pypi/forests.theme',
+    license='GPL version 2',
+    packages=find_packages('src', exclude=['ez_setup']),
+    namespace_packages=['forests'],
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'plone.api',
+        'plone.app.contenttypes',
+        'python-redmine',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
         ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['forests'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["ZopeSkel"],
-      )
+    },
+)
