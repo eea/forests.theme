7fb95ff3e0e7d086c76e14558d2888a3493fe442
ichim-david <ichim.david@gmail.com>
2018-07-06 18:33:40 +0300

    Refs #96547 implemented frontpage as in mockup and added mobile design

--- src/forests/theme/skins/fise_theme_js/fise.js
+++ src/forests/theme/skins/fise_theme_js/fise.js
@@ -1,3 +1,6 @@
 jQuery(function($){
    $("<span class='fa fa-search fise-search-icon' />").insertAfter('.searchField');
+   if (window.innerWidth < 768) {
+      $(".frontpage_promo").insertBefore('.frontpage-content');
+   }
 });
