5d1418a9a4113f23596c3fd11fcdb8875ee7705f
ichim-david <ichim.david@gmail.com>
2018-08-10 16:11:59 +0300

    Refs #97495 added jquery slick logic to frontpage

--- src/forests/theme/skins/fise_theme_js/fise.js
+++ src/forests/theme/skins/fise_theme_js/fise.js
@@ -1,7 +1,11 @@
 jQuery(function($){
-   $("<span class='fa fa-search fise-search-icon' />").insertAfter('.searchField');
+   $("<span class='fa fa-search fise-search-icon' />").insertAfter('.LSBox .searchField');
    if (window.innerWidth < 768) {
       $(".frontpage_promo").insertBefore('.frontpage-content');
    }
+   $("#edit-bar").appendTo('.page-container');
    $("#portal-globalnav").appendTo("#portal-globalnav-wrapper");
+   $("#portal-header, #portal-footer, #portal-globalnav-wrapper").addClass('container');
+   $("#portal-columns").removeClass('row').addClass('container');
 });
+
