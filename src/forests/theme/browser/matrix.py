e569d8a9ff98231dc369b3fbe3575af20e28266f
ichim-david <ichim.david@gmail.com>
2018-11-27 18:49:34 +0200

    Refs #100400 add ability to pass several filters to queryMatrixData and modified import matrix data to import data from all of the sheets present in the excel

--- src/forests/theme/browser/matrix.py
+++ src/forests/theme/browser/matrix.py
@@ -11,32 +11,34 @@ class ImportMatrixData(BrowserView):
 
     def __call__(self):
         context = self.context
+        if 'land-matrix' not in context.getId():
+            return "View should be called on the matrix excel file"
         site = getSite()
         anno = IAnnotations(site)
-        #anno.setDefault('matrix', {})
-        anno['matrix'] = {}
-        if not anno.get('matrix'):
-            anno['matrix'] = {}
-        matrix = anno.get('matrix')
-        mfile = context.restrictedTraverse('land-use-matrix-by-ms-from-2018-ghg-inventory-simple.xlsx')
-        fcontents =  open_workbook(file_contents=mfile.file.data)
-        sheet = fcontents.sheets()[0]
+        sheet_number = 1
+        fcontents =  open_workbook(file_contents=context.file.data)
         from_last_key = {}
-        for row in range(1, sheet.nrows):
-            fromv  = sheet.cell(row, 1).value
-            if not matrix.has_key(fromv):
-                from_last_key[fromv] = 1
-                matrix[fromv] = {}
-            matrix_from = matrix[fromv]
-            key = from_last_key[fromv]   
-            matrix_from[key] = {}
-            key_values = matrix_from[key]
-            for col in range(sheet.ncols):
-                title_column_value = sheet.cell(0,col).value
-                if type(title_column_value) == float:
-                    title_column_value = int(title_column_value)
-                key_values[title_column_value] = sheet.cell(row, col).value
-            from_last_key[fromv] += 1
+        for sheet in fcontents.sheets():
+            sheet_name = 'matrix_%s' % sheet_number
+            matrix = anno.get(sheet_name)
+            if not matrix:
+                anno[sheet_name] = {}
+            for row in range(1, sheet.nrows):
+                fromv  = sheet.cell(row, 1).value
+                if not from_last_key.has_key(fromv):
+                    from_last_key[fromv] = 1
+                    matrix[fromv] = {}
+                matrix_from = matrix[fromv]
+                key = from_last_key[fromv]   
+                matrix_from[key] = {}
+                key_values = matrix_from[key]
+                for col in range(sheet.ncols):
+                    title_column_value = sheet.cell(0,col).value
+                    if type(title_column_value) == float:
+                        title_column_value = int(title_column_value)
+                    key_values[title_column_value] = sheet.cell(row, col).value
+                from_last_key[fromv] += 1
+            sheet_number += 1
         return "Ok"
 
 
@@ -45,15 +47,24 @@ class QueryMatrixData(BrowserView):
     def __call__(self):
         site = getSite()
         anno = IAnnotations(site)
-        fromv = self.context.REQUEST.get('from')        
+        form = self.context.REQUEST.form
+        fromv = form.get('From')        
         bad_request = False
-        matrix = anno.get('matrix')
+        matrix = anno.get('matrix_1')
+        results = {}
         if not matrix  or not fromv:
             bad_request = True 
-            results = {}
         if not bad_request:
-            results = matrix.get(fromv, {})
+            form_keys = form.keys()
+            results = matrix.get(fromv, {}).copy()
+            for k,v in form.items():
+                if k != 'From':
+                    for key, value in results.items():
+                        item_category = value.get(k)
+                        if item_category and item_category != v:
+                            del results[key] 
         
-        self.context.REQUEST.response.setHeader("Content-type", "application/json")
+        self.context.REQUEST.response.setHeader("Content-type", 
+                                                "application/json")
 
         return json.dumps(results)
