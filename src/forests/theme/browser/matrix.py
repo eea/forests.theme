9d0c048b04b2d154888ee419f04fd8d7a145a73d
ichim-david <ichim.david@gmail.com>
2018-12-04 16:12:10 +0200

    Refs #100401 WIP returning json data in order to be used by jquery datatable

--- src/forests/theme/browser/matrix.py
+++ src/forests/theme/browser/matrix.py
@@ -17,18 +17,18 @@ class ImportMatrixData(BrowserView):
         site = getSite()
         anno = IAnnotations(site)
         sheet_number = 0
-        fcontents =  open_workbook(file_contents=context.file.data)
+        fcontents = open_workbook(file_contents=context.file.data)
         for sheet in fcontents.sheets():
             sheet_name = 'matrix_%s' % sheet_number
             matrix = anno.get(sheet_name)
             if not matrix:
-               anno[sheet_name] = OrderedDict()
-               matrix = anno[sheet_name]
+                anno[sheet_name] = OrderedDict()
+                matrix = anno[sheet_name]
             matrix['header'] = []
             for i in range(sheet.ncols):
                 matrix['header'].append(sheet.cell(0, i).value)
             for row in range(1, sheet.nrows):
-                fromv  = sheet.cell(row, sheet_number).value
+                fromv = sheet.cell(row, sheet_number).value
                 if not matrix.has_key(fromv):
                     matrix[fromv] = []
                 matrix_list = matrix[fromv]
@@ -50,20 +50,19 @@ class QueryMatrixData(BrowserView):
         bad_request = False
         matrix = anno.get('matrix_1')
         results = {}
-        if not matrix  or not fromv:
+        if not matrix or not fromv:
             bad_request = True 
         if not bad_request:
-            results = matrix.get(fromv, {}).copy()
-            for k,v in form.items():
+            rows = matrix.get(fromv, [])
+            for k, v in form.items():
                 if k != 'From':
-                    for key, value in results.items():
-                        item_category = value.get(k)
-                        if item_category and item_category != v:
-                            del results[key] 
-        
-        self.context.REQUEST.response.setHeader("Content-type", 
+                    for idx, row in enumerate(rows):
+                        if v not in row:
+                            rows.pop(idx)
+            results['columnDefs'] = matrix['header']
+            results['data'] = rows
+        self.context.REQUEST.response.setHeader("Content-type",
                                                 "application/json")
-
         return json.dumps(results)
 
     @staticmethod
@@ -72,7 +71,7 @@ class QueryMatrixData(BrowserView):
         anno = IAnnotations(site)
         matrix = anno.get('matrix_0')
         results_list = []
-        i = 0;
+        i = 0
         for value in matrix.values():
             if i == 0:
                 results_list.append(value)
