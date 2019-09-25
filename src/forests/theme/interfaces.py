96404d31f85c528a588fb23c329a616765957991
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-09-25 12:49:52 +0300

    Added configlet for mosaic styles

--- src/forests/theme/interfaces.py
+++ src/forests/theme/interfaces.py
@@ -1,8 +1,29 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
 
+from zope import schema
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+
+from forests.theme import _
+from plone.autoform import directives as form
+from plone.supermodel import model
 
 
 class IForestsThemeLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+
+class IMosaicSettings(model.Schema):
+    """ Settings for mosaic tiles
+    """
+
+    form.widget(styles='z3c.form.browser.textlines.TextLinesFieldWidget')
+    styles = schema.Set(
+        title=_(u'Styles'),
+        description=_(
+            u'Enter a list of styles to appear in the style pulldown. '
+            u'Format is title|className, one per line.'),
+        required=False,
+        default=set(["default|default-tile", ]),
+        value_type=schema.ASCIILine(title=_(u'CSS Classes')),
+    )
