e55e4bb434ef0dead228b7d01537e6dc1b61e6c8
alecghica <alecghica@gmail.com>
2020-01-09 11:19:37 +0100

    merged branches, refs #112514

--- src/forests/theme/interfaces.py
+++ src/forests/theme/interfaces.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
-"""Module where all interfaces, events and exceptions live."""
+""" Module where all interfaces, events and exceptions live.
+"""
 
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from zope.interface import Interface
 
 
 class IForestsThemeLayer(IDefaultBrowserLayer):
-    """Marker interface that defines a browser layer."""
+    """ Marker interface that defines a browser layer.
+    """
+
+
+class ILocalSectionMarker(Interface):
+    """ A local section marker. To be used with @localnavigation.
+        BBB - now registered under eea.restapi
+    """
